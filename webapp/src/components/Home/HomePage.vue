b1402681df94b7290c6b726d68c5e2b903ca36f6
anvle <51759887+anvle@users.noreply.github.com>
2024-11-06 14:58:38 -0600

    add create user page

--- webapp/src/components/Home/HomePage.vue
+++ webapp/src/components/Home/HomePage.vue
@@ -1,12 +1,12 @@
 <script setup>
-    import MyNav from '../NavBar.vue';
+    import NavBar from '../NavBar.vue';
     import HomeContent from './HomeContent.vue';
     import SCLogo from '../SCLogo.vue';
 </script>
 
 <template>
     <div id="wrapper-home">
-        <MyNav></MyNav>
+        <NavBar></NavBar>
         <HomeContent></HomeContent>
         <SCLogo></SCLogo>
     </div>
