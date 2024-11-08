60c3fb883942939079c035c826e2e9a91319f2eb
Damaris Calderon <124825496+eucalderon@users.noreply.github.com>
2024-11-08 12:08:26 -0600

    Path added for NewGroup and AddMember pages

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -3,7 +3,8 @@ import HomePage from '../components/Home/HomePage.vue'
 import LoginPage from '../components/Login/LoginPage.vue'
 import WelcomePage from '../components/Welcome/WelcomePage.vue'
 import CreateUserPage from '../components/CreateUser/CreateUserPage.vue'
-
+import NewGroupPage from '../components/NewGroup/NewGroupPage.vue'
+import AddMemberPage from '../components/AddMember/AddMemberPage.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -23,6 +24,14 @@ const router = createRouter({
         {
             path: '/create-user',
             component: CreateUserPage
+        },
+        {
+            path: '/new-group',
+            component: NewGroupPage    
+        },
+        {
+            path: '/add-member',
+            component: AddMemberPage    
         }
     ]
 })
